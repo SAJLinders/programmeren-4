6b7d290663ea5034bf50bcbf001da7ff02aad42b
Sjoerd Schepers <sjoerdschepers@hotmail.nl>
2018-04-24 11:53:37 +0200

    Added Heroku support

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const Book = require('./classes/book');
 
-let port = 3000;
+var port = process.env.port || 3000;
 
 app.get('/book', function(req, res){
     let kvk = new Book('Koning van Katoren', 'Jan Terlouw');
@@ -10,5 +10,5 @@ app.get('/book', function(req, res){
 });
 
 app.listen(port, function(){
-   console.log('Server app is listening on port 3000');
+   console.log('Server app is listening on port ' + port);
 });
